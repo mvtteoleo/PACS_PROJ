0a906960497926e643d2422500f6625f4f682842
Matteo Leone <matteo4.leone@mail.polimi.it>
2025-12-10 18:11:35 +0100

    Trying to understand the problems of convergence... Still first order

--- include/impl/MG_poisson_solver_impl.hpp
+++ include/impl/MG_poisson_solver_impl.hpp
@@ -12,6 +12,7 @@ namespace numPDE
         : r_dec{decomp}, r_BCs{Bcs}, r_const{constants}
     {
         this->build_local_dm();
+
         this->build_linear_system();
     }
 
@@ -29,7 +30,7 @@ namespace numPDE
     auto MultiGridPoissonSolver<Decomp>::build_local_dm()
     {
         PetscErrorCode ierr;
-        auto [pz, py] = r_dec.get_process_grid();
+        auto [pz, py]            = r_dec.get_process_grid();
         const auto& [nx, ny, nz] = r_dec.get_global_sizes();
 
         PetscInt NxLoc{nx - 2};
@@ -54,7 +55,6 @@ namespace numPDE
                 zl               = sizes[2];
                 zl -= static_cast<int>(is_side(SIDES::TOP, r_dec));
                 zl -= static_cast<int>(is_side(SIDES::BOTTOM, r_dec));
-
             }
             MPI_Bcast(T_info.data(), T_info.size(), MPI_INT, TOP_r, MPI_COMM_WORLD);
             lz[k] = zl;
@@ -99,6 +99,7 @@ namespace numPDE
         DMCreateGlobalVector(this->da, &b);
 
         build_int_A();
+
         apply_bc_to_A();
 
         KSPCreate(PETSC_COMM_WORLD, &ksp);
@@ -178,7 +179,7 @@ namespace numPDE
     template <DecomposeConc Decomp>
     auto MultiGridPoissonSolver<Decomp>::build_int_A()
     {
-        PetscScalar v[7];
+        PetscScalar v[7] ;
         MatStencil  row, col[7];
         row.c = 0;
 
@@ -266,8 +267,10 @@ namespace numPDE
     auto MultiGridPoissonSolver<Decomp>::apply_bc_to_A()
     {
         // Allows to change mode of modify the matrix (ADD_VALUES to INSERT_VALUES)
+        MPI_Barrier(MPI_COMM_WORLD);
         MatAssemblyBegin(A, MAT_FLUSH_ASSEMBLY);
         MatAssemblyEnd(A, MAT_FLUSH_ASSEMBLY);
+        MPI_Barrier(MPI_COMM_WORLD);
         for (auto side : enum_range<numPDE::SIDES>())
             if (is_side(side, r_dec))
             {
@@ -278,6 +281,8 @@ namespace numPDE
         MPI_Barrier(MPI_COMM_WORLD);
         MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
         MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
+//    std::cout<< "\n \n \n \n \n  after modification";
+//    MatView(A, PETSC_VIEWER_STDOUT_WORLD);
     }
 
     template <DecomposeConc Decomp>
@@ -285,36 +290,44 @@ namespace numPDE
     {
         auto info = this->get_side_infos(side);
 
-        if (info.bc == NeuHomo or info.bc == Neumann)
+        if (info.bc == BC::NeuHomo or info.bc == BC::Neumann)
         {
-            // Impose the BC Using a Polinomial.
-            // Retrieve the ghost point value by fitting a second order polinomial
+            // Impose the BC Using a Polynomial.
+            // Retrieve the ghost point value by fitting a second order polynomial
             // such that I(h) = u_1; I(2h) = u_2 and I'(0) = G;
             // Retrive: 3u_0 - 4u_1 + u_2 = -2h * G
             // The scheme is then modified to impose u_0 = 4/3u_1 - 1/3u_2 - 2hG/3
             constexpr size_t      n_appr    = 2;
-            constexpr PetscScalar v[n_appr] = {4.0 / 3.0, -1.0 / 3.0};
+            constexpr PetscScalar v[n_appr] = {4.0/3.0, -1.0/3.0};
             MatStencil            row, col[n_appr];
 
             row.c    = 0;
             col[0].c = 0;
 
-            // TODO leverage the normal attribute to enforce the BCs with a higher order
             for (auto k : info.k_range())
-                for (auto j : info.j_range())
-                    for (auto i : info.i_range())
-                    {
-                        row.i = i;
-                        row.j = j;
-                        row.k = k;
-                        for (auto el : std::views::iota(size_t{0}, n_appr))
-                        {
-                            col[el].i = i + el * info.normal[0];
-                            col[el].j = j + el * info.normal[1];
-                            col[el].k = k + el * info.normal[2];
-                        }
-                        MatSetValuesStencil(this->A, 1, &row, n_appr, col, v, ADD_VALUES);
-                    }
+            for (auto j : info.j_range())
+            for (auto i : info.i_range())
+            {
+                row.i = i;
+                row.j = j;
+                row.k = k;
+                for (auto el : std::views::iota(size_t{0}, n_appr))
+                {
+                    col[el].i = i + el * info.normal[0];
+                    col[el].j = j + el * info.normal[1];
+                    col[el].k = k + el * info.normal[2];
+                    col[el].c = 0;
+                }
+                MatSetValuesStencil(this->A, 1, &row, n_appr, col, v, ADD_VALUES);
+            }
+            return;
+        }
+        else if (info.bc == BC::Dirichlet or info.bc == BC::DirHomo) { return; }
+        else 
+        {
+            if(!r_dec.rank()) std::cerr << "\n!!! BC NOT SUPPORTED in apply_BC_A !!! \n";
+
+            return;
         }
     }
 
@@ -336,7 +349,8 @@ namespace numPDE
 
         if (info.bc == Dirichlet or info.bc == Neumann)
         {
-            const T        scale = (info.bc == BC::Dirichlet) ? -1.0 : r_const.h;
+            // Scale changes due to implementation
+            const T        scale = (info.bc == BC::Dirichlet) ? -1.0 : r_const.h*2.0/3.0;
             PetscScalar*** bAsTens;
             DMDAVecGetArray(this->da, this->b, &bAsTens);
             for (auto k : info.k_range())
@@ -379,8 +393,10 @@ namespace numPDE
                 for (PetscInt i = xs; i < xs + xm; ++i)
                 {
                     const auto pos = std::vector<T>{h * (i + 1), h * (j + 1), h * (k + 1)};
-                    x_hTens[k][j][i] =
-                        std::abs(static_cast<PetscScalar>(r_BCs.u_ex(pos)) - x_hTens[k][j][i]);
+                    const auto exact = static_cast<PetscScalar>(r_BCs.u_ex(pos));
+                    const auto num   = x_hTens[k][j][i];
+                    const auto err = std::abs( exact - num );
+                    x_hTens[k][j][i] = err ;
                 }
         DMDAVecRestoreArray(this->da, check, &x_hTens);
         VecAssemblyBegin(check);
@@ -496,6 +512,11 @@ namespace numPDE
             info.fun       = r_BCs.g_bottom;
             info.offset[2] = 0;
             info.normal    = {0, 0, 1};
+        }
+        else
+        {
+            if(!r_dec.rank())
+                std::cerr << "\n!!! Invalid SIDE in get_side_infos() !!!";
         }
 
         return info;
