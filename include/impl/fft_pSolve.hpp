b5c847c4177abfc82d6c468c7ad09acefd4ce08b
Matteo Leone <matteo4.leone@mail.polimi.it>
2025-12-12 17:41:27 +0100

    Working test (Quite dumb for the moment, but is a good starting point)

--- include/impl/fft_pSolve.hpp
+++ include/impl/fft_pSolve.hpp
@@ -200,6 +200,7 @@ namespace numPDE
     template <typename T>
     void PressureSolver<SolvePolicy::Fourier, NewDecomp<T>>::test_p_corr(bool verbose)
     {
+        this->allocate_P();
         const auto  h     = this->r_const.h;
         const auto& sizes = this->r_dec.xSize();
         const auto& nx    = sizes[0];
@@ -212,9 +213,12 @@ namespace numPDE
 
         auto idx = [&](auto i, auto j, auto k) { return i + nx * (j + ny * k); };
         auto strt = this->r_dec.xStartWGhosts();
-        for (const auto [k, j, i] : this->m_P_ghosted.int_elems())
+
+        for (const auto [k, j, i] : this->mo_P->all_elems())
         {
-            const size_t l       = idx(i, j - j_g, k - k_g);
+            const size_t l       = idx(i, j, k);
+        
+            assert(l==this->mo_P->get_linear_index(i, j, k) && "Error in the index computation");
 
             T iG = static_cast<T>(strt[0] + i);
             T jG = static_cast<T>(strt[1] + j);
@@ -223,26 +227,27 @@ namespace numPDE
             this->m_P_ghosted[l] = this->r_BCs.f(pos);
         }
 
-        this->compute_div_on_sides();
+        // this->compute_div_on_sides();
 
         // Feed the tensor to the solve method
         this->solve(this->m_P_ghosted, this->m_P_ghosted, verbose);
 
         // UPDATE V
-        const auto slice = nx * ny;
 
+    /*
+        const auto slice = nx * ny;
         for (int k = nz - 1; k > 0; k--)
         {
             std::copy_n(this->m_P_ghosted.ptr_at(idx(0, 0, k - 1)), slice,
                         this->m_P_ghosted.ptr_at(0, j_g, k_g + k - 1));
         }
         this->r_dec.exchange_ghosts(m_P_ghosted);
+    */
         
-        this->allocate_P();
         
         // Fill P with  with the solution
-        for(auto [k, j, i] : this->mo_P->all_elems())
-            this->mo_P->at(i, j, k) = this->m_P_ghosted(i, j+j_g, k+k_g);
+        for(auto  l : this->mo_P->all_linear_elements())
+            (*this->mo_P)[l] = this->m_P_ghosted[l];
         
         this->check_sol();
     }
