8d4639624c05b196af53ed92cbddb91ee5bcaeef
Matteo Leone <matteo4.leone@mail.polimi.it>
2025-12-12 22:07:51 +0100

    Working data transfer, now I do it with copy_backwardfg

--- include/impl/fft_pSolve.hpp
+++ include/impl/fft_pSolve.hpp
@@ -260,24 +260,24 @@ namespace numPDE
         // Feed the tensor to the solve method
         this->solve(this->m_P_ghosted, this->m_P_ghosted, verbose);
 
-        // Solve is OK, NOW there is the need to copy back into the relative staggered position
-        std::copy_n(beg, this->mo_P->size(), this->mo_P->begin());
+        // Solve is OK this way!
+        // std::copy_n(beg, this->mo_P->size(), this->mo_P->begin());
         /*
-            const auto slice = nx * ny;
-            for (int k = nz - 2; k >= 0; k--)
-            {
-                std::copy_n(this->m_P_ghosted.ptr_at(k*slice), slice,
-                            this->m_P_ghosted.ptr_at(0, j_g, k_g + k));
-            }
-
-            // this->r_dec.exchange_ghosts(m_P_ghosted);
-
-            // Fill P with  with the solution
-
-            for(auto [k, j, i] : this->mo_P->all_elems())
-                        this->mo_P->at(i, j, k) = this->m_P_ghosted(i, j + j_g, k + k_g);
+        * NOW there is the need to copy back into the relative staggered position
         */
-
+            // this->r_dec.exchange_ghosts(m_P_ghosted);
+        
+    for(int kp=nz-1; kp>=0; --kp)
+        for(int jp=ny-1; jp>=0; --jp)
+            for(int ip=nx-1; ip>=0; --ip)
+            {
+                const auto l = idx(ip,  jp, kp);
+                m_P_ghosted(ip,  jp+j_g, kp+k_g) = m_P_ghosted[l];
+            }
+            for(auto [k, j, _] : this->mo_P->all_elems())
+                std::copy_n(this->m_P_ghosted.ptr_at(0,  j_g + j, k + k_g), nx,
+                            this->mo_P->ptr_at(0, j,k));
         this->check_sol();
+
     }
 }; // namespace numPDE
