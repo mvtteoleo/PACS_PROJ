f8d9fdeac749ef18f4c46257692a35b55ec519f9
Matteo Leone <matteo4.leone@mail.polimi.it>
2026-01-21 01:05:42 +0100

    Fixed minor issue

--- include/impl/fft_pSolve.hpp
+++ include/impl/fft_pSolve.hpp
@@ -28,7 +28,6 @@ namespace numPDE
 
         // Update the neighbours
         this->compute_div_on_sides();
-        this->r_dec.exchange_ghosts(m_P_ghosted);
 
         this->reorder_data<MOVE_TYPE::ToNonGhosted>();
 
@@ -42,22 +41,24 @@ namespace numPDE
            
             this->check_sol();
 #endif
+
         this->reorder_data<MOVE_TYPE::ToGhosted>();
+        this->compute_div_on_sides();
 
         this->r_dec.exchange_ghosts(m_P_ghosted);
-        const auto& sz     = this->m_P_ghosted.get_sizes();
-        const auto  k_full = std::views::iota(size_t{0}, static_cast<size_t>(sz[2] - 1));
-        const auto  j_full = std::views::iota(size_t{0}, static_cast<size_t>(sz[1] - 1));
-        const auto  i_full = std::views::iota(size_t{0}, static_cast<size_t>(sz[0] - 1));
+        const auto& sz = this->m_P_ghosted.get_sizes();
+        const auto k_full = std::views::iota(size_t{0}, static_cast<size_t>(sz[2] -1));
+        const auto j_full = std::views::iota(size_t{0}, static_cast<size_t>(sz[1] -1));
+        const auto i_full = std::views::iota(size_t{0}, static_cast<size_t>(sz[0] -1));
         for (auto [k, j, i] : std::views::cartesian_product(k_full, j_full, i_full))
         {
             const auto dP = grad(m_P_ghosted, i, j, k, h);
             V(i, j, k)    = V(i, j, k) - dt_step * dP;
         }
 
-        this->r_dec.exchange_ghosts(m_P_ghosted);
         // Update P
         P = P + m_P_ghosted;
+        this->r_dec.exchange_ghosts(m_P_ghosted);
         this->r_dec.exchange_ghosts(P);
         this->r_dec.exchange_ghosts(V);
     }
@@ -340,3 +341,4 @@ namespace numPDE
         auto err = this->check_sol();
     }
 }; // namespace numPDE
+
